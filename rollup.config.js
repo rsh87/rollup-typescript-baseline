3ecf2e4e23c2ac5864341ec8455dba39bb4ca92a
rsh87 <runesejer87@gmail.com>
2019-08-03 12:39:46 +0200

    use iife format

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import typescript from 'rollup-plugin-typescript2';
-import livereload from 'rollup-plugin-livereload';
-import serve from 'rollup-plugin-serve';
-import postcss from 'rollup-plugin-postcss';
-import postcssImport from 'postcss-import';
+import typescript from "rollup-plugin-typescript2";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+import postcss from "rollup-plugin-postcss";
+import postcssImport from "postcss-import";
 
 export default {
-    input: ['./src/main.ts'],
-    output: {
-        format: 'esm',
-        dir: 'dist',
-    },
+  input: ["./src/main.ts"],
+  output: {
+    format: "iife",
+    dir: "dist"
+  },
 
-    plugins: [
-        typescript(),
-        postcss({ plugins: [postcssImport] }),
-        serve({
-            contentBase: 'dist',
-            open: true,
-        }),
-        livereload({ watch: 'dist' }),
-    ],
+  plugins: [
+    typescript(),
+    postcss({ plugins: [postcssImport] }),
+    serve({
+      contentBase: "dist",
+      open: true
+    }),
+    livereload({ watch: "dist" })
+  ]
 };
